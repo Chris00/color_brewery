ed45cb0848675155969b6d411e5c87302c4aa12d
Christophe Troestler <Christophe.Troestler@umons.ac.be>
2021-02-18 18:57:11 +0100

    Show the grayscale conversion of gradients

--- tests/gradient.ml
+++ tests/gradient.ml
@@ -2,21 +2,32 @@
 
 open Printf
 
+let table_of_colors fh ?(w=30) colors =
+  fprintf fh "<table style=\"border: 0px;  border-spacing: 0px\"><tr>\n";
+  List.iter (fun c ->
+      fprintf fh "  <td style=\"width: %dpx; height: 30px; \
+                  background-color: %s\"></td>\n"
+        w (Color_brewery.to_string c))
+    colors;
+  fprintf fh "</tr><tr>";
+  List.iter (fun c ->
+      fprintf fh "  <td style=\"width: %dpx; height: 12px; \
+                  background-color: %s\"></td>\n"
+        w (Color_brewery.(to_string (to_gray c))))
+    colors;
+  fprintf fh "</tr></table><br/>\n"
+
+(** Returns a list containing [[a;...,b]]. *)
+let rec int_range a b = if b < a then [] else a :: int_range (a+1) b
 
 let () =
   let fh = open_out "gradient.html" in
   fprintf fh "<html>\n\
               <body>\n";
-  let range ?(w=30) color ~n =
-    fprintf fh "<table style=\"border: 0px;  border-spacing: 0px\"><tr>\n";
+  let range ?w color ~n =
     let dt = 1. /. float(n - 1) in
-    for i = 0 to n - 1 do
-      let c = color (float i *. dt) in
-      fprintf fh "  <td style=\"width: %dpx; height: 30px; \
-                  background-color: %s\"></td>\n"
-        w (Color_brewery.to_string c)
-    done;
-    fprintf fh "</tr></table><br/>\n";
+    let colors = List.map (fun i -> color (float i *. dt)) (int_range 0 n) in
+    table_of_colors fh ?w colors;
   in
   let gradient ?w c0 c1 ~n =
     let g = Color_brewery.(Gradient.v (of_int_exn c0) (of_int_exn c1)) in
